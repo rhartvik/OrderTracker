3721e6d64b38dc9a7ead3bd81157433b3ea7d31e
RACHEL HARTVIKSEN <rhartviksen@gmail.com>
2017-10-18 22:39:27 -0400

    Adjust spacing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Cart from './Components/Cart'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
 registerServiceWorker();
